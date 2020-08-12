1dc51ceb514d90049be3b7a37110c64b1b40bfce
Marta Geraldo <60014891+martageraldo@users.noreply.github.com>
2020-08-12 14:49:30 -0300

    12/08

---
+++ aula9/ex01h.py
@@ -0,0 +1,6 @@
+frase = 'Curso em Vídeo Python'
+print(frase[::2])
+
+print('Oi')
+
+print(""" Dois-pontos é um sinal de pontuação que anuncia uma citação, uma enumeração, um esclarecimento, ou ainda uma síntese do que se acabou de dizer. Corresponde a uma pausa breve da linguagem oral e a uma entoação descendente. Em matemática os dois-pontos são utilizados como símbolo da divisão.""")
