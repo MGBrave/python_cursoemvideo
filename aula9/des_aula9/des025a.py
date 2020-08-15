b2f72ffa45189ea0eae9e11e5c0eff377117ada7
Marta Geraldo <60014891+martageraldo@users.noreply.github.com>
2020-08-15 20:00:06 -0300

    15-08

--- aula9/des_aula9/des025a.py
+++ aula9/des_aula9/des025a.py
@@ -1,6 +1,6 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
 nome = str(input('Qual é o seu nome completo? ')).strip()
-print('Seu nome tem Silva? {}'.format(nome[:5] == 'Silva'))
+print('Seu nome tem Silva? {}'.format('Silva' in nome))
 
 
 
--- aula9/des_aula9/des025d.py
+++ aula9/des_aula9/des025d.py
@@ -1,6 +1,7 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
 nome = str(input('Qual é o seu nome completo? ')).strip()
-print('Seu nome tem Silva? {}'.format(nome[:5] == 'Silva'))
+print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
+#procurar o nome em minusculo
 
 
 
